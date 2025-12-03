10a8c35d5214f1619c1217dcd519d7a8a7239394
SumanPoU <acharyas186@gmail.com>
2025-12-03 07:13:52 +0545

    ::detect:: env on vercel

--- src/app/api/transaction/route.ts
+++ src/app/api/transaction/route.ts
@@ -25,6 +25,12 @@ async function loadTransactions(): Promise<Transaction[]> {
 
 // Save JSON file
 async function saveTransactions(transactions: Transaction[]) {
+  if (process.env.VERCEL) {
+    return NextResponse.json(
+      { success: false, message: "File writing not supported on Vercel" },
+      { status: 500 }
+    );
+  }
   const filePath = path.join(
     process.cwd(),
     "public",
